452f7ae3415141f1816450f1905b42de5816bdd7
aylenalderete <aylenalderete@hotmail.com>
2022-01-25 11:46:45 -0300

    MNIST digits recognition

--- Supervised learning with scikit-learn/K-nearest neighbors.py
+++ Supervised learning with scikit-learn/K-nearest neighbors.py
@@ -1,10 +1,54 @@
-# k-Nearest Neighbors: Fit
-from sklearn.neighbors import KNeighborsClassifier
+# The MNIST digits recognition dataset
+from sklearn import datasets
+import matplotlib.pyplot as plt
 
-# X and y are a DataFrame and Series respectively; the scikit-learn API
-# will accept them in this form also as long as they are of the right shape.
-y = df['party'].values
-X = df.drop('party', axis=1).values
+digits = datasets.load_digits()
 
-knn = KNeighborsClassifier(n_neighbors=6)
-knn.fit(X, y)
+print(digits.keys())
+print(digits.DESCR)
+
+print(digits.images.shape)
+print(digits.data.shape)
+
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+
+
+# ---------------------------------------------------
+# Train/Test Split + Fit/Predict/Accuracy
+from sklearn.neighbors  import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+X = list(digits.data)
+y = list(digits.target)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
+
+knn = KNeighborsClassifier(n_neighbors=7)
+knn.fit(X_train, y_train)
+
+print(knn.score(X_test, y_test))
+
+
+
+# --------------------------------------
+# Overfitting and underfitting
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+for i, k in enumerate(neighbors):
+    knn = KNeighborsClassifier(n_neighbors= k)
+    knn.fit(X_train, y_train)
+    train_accuracy[i] = knn.score(X_train, y_train)
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
